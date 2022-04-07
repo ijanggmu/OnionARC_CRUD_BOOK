58b5d309fe526778c11e56a964648c20b6ba4027
Ejan Shrestha <sthaejan00@gmail.com>
2022-04-07 04:47:52 -0700

    Create Create Function

--- Book/Controllers/BookController.cs
+++ Book/Controllers/BookController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OA.Data;
 using OA.Service;
+using OA.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OA.Web.Controllers
 {
@@ -10,9 +15,47 @@ namespace OA.Web.Controllers
         {
             _bookServices = bookService;
         }
+        [HttpGet]
         public IActionResult Index()
+        {
+            List<BookViewModel> model = new List<BookViewModel>();
+            _bookServices.GetBooks().ToList().ForEach(u => {
+                BookViewModel book = new BookViewModel()
+                {
+                    Id = u.Id,
+                    Name = u.Name,
+                    Author = u.Author,
+                    ISBN = u.ISBN
+                };
+                model.Add(book);
+            });
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(BookViewModel m)
+        {
+            
+            
+                BookList b = new BookList()
+                {
+                    Name=m.Name,
+                    Author=m.Author,
+                    ISBN=m.ISBN,
+                    AddedDate=DateTime.UtcNow,
+                    ModifiedDate=DateTime.UtcNow
+                    
+                };
+                _bookServices.InsertBook(b);
+            
+
+            return View();
+        }
+
+
     }
 }
---
+++ Book/Models/BookViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OA.Web.Models
+{
+    public class BookViewModel
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
